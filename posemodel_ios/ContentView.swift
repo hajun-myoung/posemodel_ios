6f4b8cf28fc0232e7144c5732947ebcf25bd0a40
Denver <audgkwns44@gmail.com>
2024-01-12 14:48:42 +0900

    [Create]Resizing without keep the ratio

--- posemodel_ios/ContentView.swift
+++ posemodel_ios/ContentView.swift
@@ -11,6 +11,8 @@ import UIKit
 let testImage:UIImage = UIImage(named: "testimage")!
 
 struct ContentView: View {
+    @State var resizedImage:UIImage? = nil
+    
     var body: some View {
         VStack {
             Text("Give Image: testImage")
@@ -19,6 +21,25 @@ struct ContentView: View {
             Image(uiImage: testImage)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+            
+            Button {
+                resizedImage = testImage.resized(to: CGSize(width: 320, height: 320))
+            } label: {
+                Label("Resize Image to 320*320", systemImage: "square.arrowtriangle.4.outward")
+                    .font(.system(size: 24, weight: .bold))
+            }
+            .padding()
+            
+            if let resizedImage {
+                Image(uiImage: resizedImage)
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+                      .padding()
+            } else {
+                Text("No Resized Image is Loaded Yet")
+                    .font(.system(size: 20, design: .serif))
+            }
+            
         }
         .padding()
     }
---
+++ posemodel_ios/Extensions/UIImageExtension.swift
@@ -0,0 +1,17 @@
+//
+//  resizing.swift
+//  posemodel_ios
+//
+//  Created by 명하준 on 1/12/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: CGPointZero, size: size))
+        }
+    }
+}
