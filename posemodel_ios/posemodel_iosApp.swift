7ffc4bc072b5f164d4552b027ba3d5c16b04968d
Denver <audgkwns44@gmail.com>
2024-01-12 11:27:09 +0900

    Initial Commit

---
+++ posemodel_ios/posemodel_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  posemodel_iosApp.swift
+//  posemodel_ios
+//
+//  Created by 명하준 on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct posemodel_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
