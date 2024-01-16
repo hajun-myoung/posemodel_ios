5185ce7a8b5cda2f126e7061ef7024c7726858cb
Denver <audgkwns44@gmail.com>
2024-01-16 16:04:24 +0900

    [Create]Model Loader, init model

--- posemodel_ios/Analysers/HandlePosemodel.swift
+++ posemodel_ios/Analysers/HandlePosemodel.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+import MLKitPoseDetectionAccurate
+
+class PoseEstimator {
+    private var poseDetector: PoseDetector
+    
+    init() {
+        let options = AccuratePoseDetectorOptions()
+        options.detectorMode = .singleImage
+        
+        self.poseDetector = PoseDetector.poseDetector(options: options)
+    }
+}
