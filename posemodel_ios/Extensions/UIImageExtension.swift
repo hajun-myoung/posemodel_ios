d88b7a3f31046d9a8b98964fc0b53e88bf8b5a26
Denver <audgkwns44@gmail.com>
2024-01-16 16:03:33 +0900

    [Delete]Read image from the PixelBuffer

--- posemodel_ios/Extensions/UIImageExtension.swift
+++ posemodel_ios/Extensions/UIImageExtension.swift
@@ -10,17 +10,6 @@ import UIKit
 import VideoToolbox
 
 extension UIImage {
-    public convenience init?(pixelBuffer: CVPixelBuffer) {
-        var cgImage: CGImage?
-        VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &cgImage)
-        
-        guard let cgImage = cgImage else {
-            return nil
-        }
-        
-        self.init(cgImage: cgImage)
-    }
-
     func resized(to size: CGSize) -> UIImage {
         return UIGraphicsImageRenderer(size: size).image { _ in
             draw(in: CGRect(origin: CGPointZero, size: size))
