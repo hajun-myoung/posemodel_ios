6c713241ee3deb745d0bfd4eb4eeacc8003578c6
Denver <audgkwns44@gmail.com>
2024-01-16 16:04:10 +0900

    [Update]Image Converter - UI to Vision

--- posemodel_ios/ContentView.swift
+++ posemodel_ios/ContentView.swift
@@ -7,14 +7,12 @@
 
 import SwiftUI
 import UIKit
+import MLKitVision
 
 let testImage:UIImage = UIImage(named: "test001_02")!
-var pixelBuffer: CVPixelBuffer? = nil
 
 struct ContentView: View {
-    @State var resizedImage:UIImage? = nil
-    @State var isPixelbufferConverted:Bool = false
-    
+    @State var isVisionImageConverted: Bool = false
     var body: some View {
         ScrollView{
             VStack {
@@ -31,42 +29,22 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                 
                 Button {
-                    resizedImage = testImage.resized(to: CGSize(width: 256, height: 256))
+                    let vImage = VisionImage(image: testImage)
+                    vImage.orientation = testImage.imageOrientation
+                    isVisionImageConverted = true
                 } label: {
-                    Label("Resize Image to 256*256", systemImage: "square.arrowtriangle.4.outward")
+                    Label("Convert to VisionImage", systemImage: "sunglasses")
                         .font(.system(size: 24, weight: .bold))
                 }
                 .padding()
                 
-                if let resizedImage {
-                    Image(uiImage: resizedImage)
-                          .resizable()
-                          .aspectRatio(contentMode: .fit)
-                          .padding()
-                } else {
-                    Text("No Resized Image is Loaded Yet")
+                if isVisionImageConverted {
+                    Text("UIImage has been converted to VisionImage")
                         .font(.system(size: 20, design: .serif))
-                        .padding()
-                }
-                
-                Button {
-                    pixelBuffer = buffer(from: resizedImage!)
-                    isPixelbufferConverted = true
-                } label: {
-                    Label("Convert to CVPixelBuffer", systemImage: "square.arrowtriangle.4.outward")
-                        .font(.system(size: 24, weight: .bold))
-                }
-                .padding()
-                
-                if isPixelbufferConverted {
-                    Text("State: Converted")
-                        .font(.system(size: 12, design: .serif))
-                        .underline()
                 }
                 else {
-                    Text("State: Not Converted Yet")
-                        .font(.system(size: 12, design: .serif))
-                        .underline()
+                    Text("Not converted, Yet")
+                    .font(.system(size: 20, design: .serif))
                 }
                 
             }
