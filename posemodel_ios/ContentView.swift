c730292384fb8fa0bf85d687cd5295817860850d
Denver <audgkwns44@gmail.com>
2024-01-16 16:31:35 +0900

    [Update]ContentView ModelLoad Handler

--- posemodel_ios/ContentView.swift
+++ posemodel_ios/ContentView.swift
@@ -10,9 +10,11 @@ import UIKit
 import MLKitVision
 
 let testImage:UIImage = UIImage(named: "test001_02")!
+var poseEstimationModel: PoseEstimator? = nil
 
 struct ContentView: View {
     @State var isVisionImageConverted: Bool = false
+    @State var isModelLoaded: Bool = false
     var body: some View {
         ScrollView{
             VStack {
@@ -29,6 +31,25 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                 
                 Button {
+                    poseEstimationModel = PoseEstimator()
+                    isModelLoaded = true
+                } label: {
+                    Label("Load Model", systemImage: "arrow.down.app.fill")
+                        .font(.system(size: 24, weight: .bold))
+                }
+                
+                if isModelLoaded {
+                    Text("The Model is Successfully Loaded")
+                        .font(.system(size: 20, design: .serif))
+                        .padding()
+                }
+                else {
+                    Text("Model is not Loaded")
+                    .font(.system(size: 20, design: .serif))
+                    .padding()
+                }
+                
+                Button {
                     let vImage = VisionImage(image: testImage)
                     vImage.orientation = testImage.imageOrientation
                     isVisionImageConverted = true
@@ -36,15 +57,16 @@ struct ContentView: View {
                     Label("Convert to VisionImage", systemImage: "sunglasses")
                         .font(.system(size: 24, weight: .bold))
                 }
-                .padding()
                 
                 if isVisionImageConverted {
                     Text("UIImage has been converted to VisionImage")
                         .font(.system(size: 20, design: .serif))
+                        .padding()
                 }
                 else {
                     Text("Not converted, Yet")
-                    .font(.system(size: 20, design: .serif))
+                        .font(.system(size: 20, design: .serif))
+                        .padding()
                 }
                 
             }
